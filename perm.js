54306b2948ae51cfa1345f085f97bdb4d34bc267
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2018-03-29 17:30:33 +0300

    Update perm.js

--- perm.js
+++ perm.js
@@ -35,7 +35,7 @@ client.on('message', message => {
 
 // playing
 client.on('ready', () => {
-                                                                                                                                                                                                                                                                              client.user.setGame(`/help | atomicbot | تم الاشتراك ببوت البرميوم | Created By PITAR`,'https://www.twitch.tv/v5bz');
+                                                                                                                                                                                                                                                                              client.user.setGame(`f!help | UNAVAILABLE | تم الاشتراك ببوت البرميوم | Created By PITAR`,'https://www.twitch.tv/v5bz');
 });
 
 
